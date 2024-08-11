0fa31867df15dc3160fea9e585112e73e99b70a1
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-08-11 09:04:23 +0530

    account schema completed

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const zod = require('zod')
-const User = require("../db")
+const { User } = require("../db")
 const JWT_SECRET = require("../config")
 const jwt = require("jsonwebtoken")
 const { authMiddleware } = require("../middleware/middleware")
