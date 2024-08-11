110c6c27b92eede6cc16c2e22a63fceca31f8ce9
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-08-11 10:45:24 +0530

    balance api done to fetch balance

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,4 +1,15 @@
 const express = require('express')
+const { Account } = require('../db')
+const { authMiddleware } = require('../middleware/middleware')
 const router = express.Router()
+
+
+router.get("/balance", authMiddleware, async (req, res) => {
+
+    const account = await Account.findOne({ userId: req.userId })
+
+    res.json({ balance: account.balance })
+})
+
 
 module.export = router
