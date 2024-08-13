fd094947f435cfc0de22d57dd2773277507323ff
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-08-13 08:35:16 +0530

    App completion

--- frontend/src/Component/Appbar.jsx
+++ frontend/src/Component/Appbar.jsx
@@ -1,10 +1,14 @@
 const Appbar = () => {
   return (
-    <div className="flex justify-between shadow-md px-10  rounded-lg py-3 font-mono text-2xl">
+    <div className="flex justify-between items-center shadow px-10  rounded-lg py-3 font-mono text-2xl">
       <h1 className="font-bold">Paytm App</h1>
-      <div className="flex justify-between gap-6 font-semibold">
+      <div className="flex justify-between items-center gap-6 font-semibold">
         <h3>Hello</h3>
-        <div>u</div>
+        <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
+                <div className="flex flex-col justify-center h-full text-xl">
+                    U
+                </div>
+            </div>
       </div>
     </div>
   );
